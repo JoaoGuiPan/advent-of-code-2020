e60a4aa9ea1c2b034086e60a33f6cba4750daccc
Joao Guilherme Pan <joaoguilhermepan@gmail.com>
2021-07-18 21:19:34 +0200

    Part 2 - 4D

--- day17/ConwayCubes.java
+++ day17/ConwayCubes.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class ConwayCubes {
 
     private static final int FIRST_CYCLE = 0;
     private static final int FINAL_CYCLE = 6;
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(final String []args) {
 
-        // TEST SCENARIO
+        // TEST SCENARIO - 112 on 3d and 848 on 4d
 //        final String[][] xyCoordinates = {
 //                {".","#","."},
 //                {".",".","#"},
@@ -46,7 +46,7 @@ public class Main {
         for (int y = 0; y < yxCoordinates.length; y++) {
             for (int x = 0; x < yxCoordinates[y].length; x++) {
                 final String value = yxCoordinates[y][x];
-                coordinates.put(new Coordinates(x,y,0), ACTIVE.equalsIgnoreCase(value));
+                coordinates.put(new Coordinates(x,y,0,0), ACTIVE.equalsIgnoreCase(value));
             }
         }
         return coordinates;
@@ -62,39 +62,48 @@ public class Main {
         int minX = 0;
         int minY = 0;
         int minZ = 0;
+        int minW = 0;
 
         int maxX = 0;
         int maxY = 0;
         int maxZ = 0;
+        int maxW = 0;
 
         for (final Coordinates coordinate : coordinates.keySet()) {
             int x = coordinate.getX();
             int y = coordinate.getY();
             int z = coordinate.getZ();
+            int w = coordinate.getW();
 
             minX = Math.min(x, minX);
             minY = Math.min(y, minY);
             minZ = Math.min(z, minZ);
+            minW = Math.min(w, minW);
 
             maxX = Math.max(x, maxX);
             maxY = Math.max(y, maxY);
             maxZ = Math.max(z, maxZ);
+            maxW = Math.max(w, maxW);
         }
 
         minX--;
         minY--;
         minZ--;
+        minW--;
 
         maxX++;
         maxY++;
         maxZ++;
+        maxW++;
 
         final Map<Coordinates, Boolean> newCoordinates = new HashMap<>();
 
         for (int x = minX; x <= maxX; x++) {
             for (int y = minY; y <= maxY; y++) {
                 for (int z = minZ; z <= maxZ; z++) {
-                    updateCoordinates(coordinates, newCoordinates, x, y, z);
+                    for (int w = minW; w <= maxW; w++) {
+                        updateCoordinates(coordinates, newCoordinates, x, y, z, w);
+                    }
                 }
             }
         }
@@ -104,9 +113,9 @@ public class Main {
 
     private static void updateCoordinates(final Map<Coordinates, Boolean> oldCoordinates,
                                           final Map<Coordinates, Boolean> newCoordinates,
-                                          final int x, final int y, final int z) {
+                                          final int x, final int y, final int z, final int w) {
 
-        final Coordinates coordinate = new Coordinates(x, y, z);
+        final Coordinates coordinate = new Coordinates(x, y, z, w);
         final boolean isActive = oldCoordinates.getOrDefault(coordinate, false);
         int activeNeighbours = countActiveNeighbours(coordinate, oldCoordinates);
 
@@ -120,7 +129,7 @@ public class Main {
     }
 
     /**
-     * Each cube has 9(z-1) + 8(z) + 9(z+1) = 26 neighbours due to its position on a 3d plane.
+     * Each cube has 26 neighbours on 3d and 80 neighbours on 4d (ignores itself)
      */
     private static int countActiveNeighbours(final Coordinates coordinate, final Map<Coordinates, Boolean> coordinates) {
         int activeNeighbours = 0;
@@ -128,21 +137,24 @@ public class Main {
         int minX = coordinate.getX() - 1;
         int minY = coordinate.getY() - 1;
         int minZ = coordinate.getZ() - 1;
+        int minW = coordinate.getW() - 1;
 
         int maxX = coordinate.getX() + 1;
         int maxY = coordinate.getY() + 1;
         int maxZ = coordinate.getZ() + 1;
+        int maxW = coordinate.getW() + 1;
 
         for (int x = minX; x <= maxX; x++) {
             for (int y = minY; y <= maxY; y++) {
                 for (int z = minZ; z <= maxZ; z++) {
+                    for (int w = minW; w <= maxW; w++) {
+                        final Coordinates neighbour = new Coordinates(x,y,z,w);
 
-                    final Coordinates neighbour = new Coordinates(x,y,z);
-
-                    if (coordinate.isValidNeighbour(neighbour)) { // ignores itself, hence 8(z) instead of 9(z)
-                        final Boolean isActive = coordinates.get(neighbour);
-                        if (isActive != null && isActive) {
-                            activeNeighbours++;
+                        if (coordinate.isValidNeighbour(neighbour)) { // ignores itself
+                            final Boolean isActive = coordinates.get(neighbour);
+                            if (isActive != null && isActive) {
+                                activeNeighbours++;
+                            }
                         }
                     }
                 }
--- day17/Coordinates.java
+++ day17/Coordinates.java
@@ -6,11 +6,13 @@ public class Coordinates {
     private final int x;
     private final int y;
     private final int z;
+    private final int w;
 
-    public Coordinates(int x, int y, int z) {
+    public Coordinates(final int x, final int y, final int z, final int w) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.w = w;
     }
 
     public int getX() {
@@ -25,10 +27,15 @@ public class Coordinates {
         return z;
     }
 
+    public int getW() {
+        return w;
+    }
+
     public boolean isValidNeighbour(final Coordinates neighbour) {
         return this.x != neighbour.getX()
                 || this.y != neighbour.getY()
-                || this.z != neighbour.getZ();
+                || this.z != neighbour.getZ()
+                || this.w != neighbour.getW();
     }
 
     @Override
@@ -36,16 +43,16 @@ public class Coordinates {
         if (this == o) return true;
         if (!(o instanceof Coordinates)) return false;
         Coordinates that = (Coordinates) o;
-        return getX() == that.getX() && getY() == that.getY() && getZ() == that.getZ();
+        return getX() == that.getX() && getY() == that.getY() && getZ() == that.getZ() && getW() == that.getW();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getX(), getY(), getZ());
+        return Objects.hash(getX(), getY(), getZ(), getW());
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(new int[]{x,y,z});
+        return Arrays.toString(new int[]{x,y,z,w});
     }
 }
